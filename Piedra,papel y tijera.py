afc8f332f509bb64eb59a4cde4e1c81d0f9de4aa
Fede.M <prueba@correo.com>
2023-08-20 20:05:37 -0300

    Carga total del proyecto

--- Piedra,papel y tijera.py
+++ Piedra,papel y tijera.py
@@ -1,42 +1,58 @@
+# Importamos el modulo random
 import random
 
-# los dos contadores 
+# Inicializamos los dos contadores 
 Human_c = 0
 Maquina_c = 0
 
+# Lista de seleccion para humano y maquina
 h_game = ["Papel","Piedra","Tijera"]
 m_game = ["Papel","Piedra","Tijera"]
 
-
+# Variable de juego, mientras sea True el juego se inicia. 
 jugar = True
 
-while jugar:
+print ("empieza el juego: ")
+print (" \n -------------------------- \n ")
+
+#Iniciamos el juego. 
+while jugar: 
 
     h_choice = random.choice(h_game)
     m_choice = random.choice(m_game)
 
-    print ("empieza el juego: ")
-    print (" \n -------------------------- \n ")
 
-    if (h_choice == "Papel" and m_choice == "Piedra") or (h_choice == "Piedra" and m_choice == "Tijera") or (h_choice == "Tijera" and m_choice == "Papel"):
-        print (f"Humano eligio {h_choice}, Maquina eligio {m_choice}")
+    if ((h_choice == "Papel" and m_choice == "Piedra") or 
+        (h_choice == "Piedra" and m_choice == "Tijera") or 
+        (h_choice == "Tijera" and m_choice == "Papel")):
+
+        print (f"Humano eligio {h_choice}, Maquina eligio {m_choice}, Humano gana 1 punto.")
         Human_c += 1
+        print (Human_c, "puntos acumulados.")
+        print (" \n -------------------------- \n ")
 
     elif h_choice == m_choice:
-        print ("Empate")
+        print ("Empate, no gana ninguno de los dos.\n","\bHumano pts ->", Human_c,"\nMaquina pts ->", Maquina_c)
+        print (" \n -------------------------- \n ")
     
     else:
-        print (f"Maquina eligio {m_choice}, Humano eligio {h_choice}")
+        print (f"Maquina eligio {m_choice}, Humano eligio {h_choice}.")
         Maquina_c += 1
+        print (Maquina_c, "puntos acumulados.")
+        print (" \n -------------------------- \n ")
     
     while Human_c == 3 or Maquina_c == 3:
-        continuar = input(print ("Desea continuar jugando ? "))
         
-        if continuar == "Y" or continuar == "y":
-            print ("Continuamos")
-        elif continuar == "N" or continuar =="n":
-            print ("Temrinamos")
-            jugar = False
-        continue
+        print (f"""
+        Juego terminado.
+        Score: 
+        Humano = {Human_c},
+        Maquina = {Maquina_c}       
+        """)
+
+        mensaje = "Gana humano."
+        print (mensaje.upper() if Human_c == 3 else "GANA MAQUINA.")
+        jugar = False
+        break
  
 
